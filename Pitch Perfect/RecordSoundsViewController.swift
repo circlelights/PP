2eb767c18c3ff667cda44b2a62b4ece7e259dd29
Jason Isler <circlelights@me.com>
2016-03-16 20:43:14 -0400

    Wanted to add all files to a remote directory

---
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -0,0 +1,91 @@
+//
+//  RecordSoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Developer2011 on 3/14/15.
+//  Copyright (c) 2015 Circlelights Co. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    
+    var audioRecorder: AVAudioRecorder!
+    var recordedAudio: RecordedAudio!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var micLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        //Hide the stop button
+        stopButton.hidden = true
+        micLabel.hidden = true
+    }
+
+
+    @IBAction func micButton(sender: UIButton) {
+        stopButton.hidden = false
+        //TODO:  Show text "recording in progress"
+        //TODO:  Record the user's voice
+   //     micLabel.text = "recording"
+        micLabel.hidden = false
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "my_audio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
+        
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "stopRecording") {
+            let playSoundsVC:PlaySoundsViewController = segue.destinationViewController as! PlaySoundsViewController
+            let data = sender as! RecordedAudio
+            playSoundsVC.receivedAudio = data
+        }
+    }
+    
+    
+    @IBAction func stopRecording(sender: UIButton) {
+        micLabel.hidden = true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+        
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        if(flag) {
+            recordedAudio = RecordedAudio()
+            recordedAudio.filePathUrl = recorder.url
+            recordedAudio.title = recorder.url.lastPathComponent
+            self.performSegueWithIdentifier("stopRecording", sender: recordedAudio)
+        } else {
+            print("Recording was not successful")
+            
+            stopButton.hidden = true
+        }
+    }
+
+}
+
---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,13 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Developer2011 on 3/6/16.
+//  Copyright © 2016 Circlelights Co. All rights reserved.
+//
+
+import Foundation
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
